f46689fab10681c205c97d5e134ef415d13973e9
Alwyn Durham <alwyn.durham@evisions.com>
2018-01-18 02:15:55 -0800

    added some more sample data

--- app/data/friends.js
+++ app/data/friends.js
@@ -5,6 +5,21 @@ var friendsArray = [
     "name": "Ahmed",
     "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
     "scores": [5, 1, 4, 4, 5, 1, 2, 5, 4, 1]
+  },
+  {
+    "name": "Alwyn",
+    "photo": "https://i.imgur.com/79J4DIY.png",
+    "scores": [5, 1, 5, 5, 4, 5, 1, 4, 1, 4]
+  },
+  {
+    "name": "Kitty",
+    "photo": "https://upload.wikimedia.org/wikipedia/commons/thumb/3/3a/Cat03.jpg/1200px-Cat03.jpg",
+    "scores": [5, 5, 5, 3, 4, 5, 1, 4, 1, 3]
+  },
+  {
+    "name": "Puppy",
+    "photo": "http://cdn1-www.dogtime.com/assets/uploads/2011/03/puppy-development-300x200.jpg",
+    "scores": [5, 1, 5, 3, 5, 5, 5, 5, 2, 1]
   }
 ];
 
