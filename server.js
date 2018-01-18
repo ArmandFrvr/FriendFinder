21c15725d78aca1d26639542441e8eeaf732788b
Alwyn Durham <alwyn.durham@evisions.com>
2018-01-18 01:01:07 -0800

    god i hate people 2 hours because bodyparser had to be extended:true

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ var bodyParser = require("body-parser");
 var app = express();
 var port = process.env.PORT || 3002;
 
-app.use(bodyParser.urlencoded({ extended: false}));
+app.use(bodyParser.urlencoded({ extended: true}));
 app.use(bodyParser.json());
 app.use(express.static("./app/public/assets"));
 
