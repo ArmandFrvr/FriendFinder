49720efcea1e28384f797074a182526ae738c4fb
Alwyn Durham <alwyn.durham@evisions.com>
2018-01-17 21:33:11 -0800

    Create server.js

--- server.js
+++ server.js
@@ -1,18 +1,22 @@
+// Dependencies
 var express = require("express");
+var bodyParser = require("body-parser");
+
+
+// Express config
 var app = express();
+var port = process.env.PORT || 3002;
+
+app.use(bodyParser.urlencoded({ extended: false}));
+app.use(bodyParser.json());
 
 
-
-app.get("/", function(req, res) {
-
-
-
+// Routes
+require("./routing/apiRoutes")(app);
+require("./routing/htmlRoutes")(app);
 
 
-
-
-  res.send();
-
+// Listener
+app.listen(port, function() {
+  console.log("App listening on port: " + PORT);
 });
-
-app.listen(3005);
